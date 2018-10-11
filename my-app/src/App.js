e672dc47b72de285830b4857c16453370ce11b20
Evan Meshberg <emeshberge@gmail.com>
2018-10-11 02:04:22 +0000

    Add ninja

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 // import './App.css';
 
 class App extends Component {
   state = {
-    ninjas : [
+    ninjas: [
       { name: 'Ryu', age: 30, belt: 'black', id: 1 },
       { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
-      { name: 'Crystal', age: 25, belt: 'pink', id: 3 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
         <h1>My first React app!</h1>
         <p>Welcome :)</p>
         <Ninjas ninjas={this.state.ninjas} /> 
+        <AddNinja />
       </div>
     );
   }
